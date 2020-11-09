25a9cad76d04d41691b7bd6335af783c1ae2f9e1
sukesh shetty <arrebhaibhaibhai1@gmail.com>
2020-11-09 12:53:49 +0530

    Add files via upload

---
+++ email1.py
@@ -0,0 +1,13 @@
+import smtplib             # pip install smtplib                         
+
+sender_email = "abc@gmail.com"
+rec_email = "xyz@gmail.com", "efg@gmail.com"         #recivers email id  
+password = input(str("pls enter your password : "))  #sender email password  
+message = "hello this message is sent using python"
+
+server = smtplib.SMTP('smtp.gmail.com', 587)      # port address 587
+server.starttls()
+server.login(sender_email, password)
+print("login success")
+server.sendmail(sender_email, rec_email, message)
+print("email has been sent to ", rec_email)
